73cd1cc3c8561e15d6332786237f2b36f6626a02
Allen Lee <looplee@msn.com>
2020-04-11 21:30:17 +0800

    Refactor the scheduling code for children of OcrScope.

--- Cloud.Ocr/Cloud.Ocr.Activities/Activities/OcrScope.cs
+++ Cloud.Ocr/Cloud.Ocr.Activities/Activities/OcrScope.cs
@@ -19,10 +19,10 @@ namespace Cloud.Ocr.Activities
         #region Properties
 
         [Browsable(false)]
-        public ActivityAction<IObjectContainer> OcrClient { get; set; }
+        public Activity Head { get; set; }
 
         [Browsable(false)]
-        public ActivityAction<IObjectContainer​> Body { get; set; }
+        public Activity Body { get; set; }
 
         /// <summary>
         /// If set, continue executing the remaining activities even if the current activity has failed.
@@ -47,16 +47,7 @@ namespace Cloud.Ocr.Activities
         {
             _objectContainer = objectContainer;
 
-            OcrClient = new ActivityAction<IObjectContainer>
-            {
-                Argument = new DelegateInArgument<IObjectContainer>(ParentContainerPropertyTag)
-            };
-
-            Body = new ActivityAction<IObjectContainer>
-            {
-                Argument = new DelegateInArgument<IObjectContainer> (ParentContainerPropertyTag),
-                Handler = new Sequence { DisplayName = Resources.Do }
-            };
+            Body = new Sequence { DisplayName = Resources.Do };
         }
 
         public OcrScope() : this(new ObjectContainer())
@@ -77,17 +68,20 @@ namespace Cloud.Ocr.Activities
 
         protected override async Task<Action<NativeActivityContext>> ExecuteAsync(NativeActivityContext context, CancellationToken cancellationToken)
         {
-            // Schedule the client to inject an IOcrClient object.
-            if (OcrClient != null && OcrClient.Handler != null)
-            {
-                context.ScheduleAction<IObjectContainer>(OcrClient, _objectContainer, OnCompleted, OnFaulted);
-            }
-
             return (ctx) => {
                 // Schedule child activities
-                if (Body != null)
+                if (Head != null && Body != null)
                 {
-                    ctx.ScheduleAction<IObjectContainer>(Body, _objectContainer, OnCompleted, OnFaulted);
+                    var children = new ActivityAction<IObjectContainer>
+                    {
+                        Argument = new DelegateInArgument<IObjectContainer>(ParentContainerPropertyTag),
+                        Handler = new Sequence
+                        {
+                            Activities = { Head, Body }
+                        }
+                    };
+
+                    ctx.ScheduleAction<IObjectContainer>(children, _objectContainer, OnCompleted, OnFaulted);
                 }
             };
         }
